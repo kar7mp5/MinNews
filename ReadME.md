954623c95c36dfc36aebe38f77ca1171b5bf87ae
kimminsum <tommy1005a@gmail.com>
2024-03-19 16:14:54 +0900

    test

---
+++ ReadME.md
@@ -0,0 +1,11 @@
+# Notion NPL Project
+
+## Algorithm
+- RNN
+- LSTM
+
+## Train Data
+### test data by kaggle
+https://www.kaggle.com/datasets/aashita/nyt-comments
+
+### crawling text data from NEWS
---
+++ test.py
@@ -0,0 +1,5 @@
+import korean_news_scraper
+
+keywords = ["news", "happy", "environment"]
+korean_news_scraper.save_article_links(keywords, "data", lang="en-EN")
+korean_news_scraper.extract_article_content("data")
