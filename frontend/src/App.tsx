9cb7c90ece17400ab70447d9dbe2e9a18b085c62
kimminsum <tommy1005a@gmail.com>
2024-05-26 23:15:40 +0900

    frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import 'App.css';
 
 import Main from 'pages/Main';
 
 const App: React.FC = () => {
-    const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
-
-    const toggleSidebar = () => {
-        setIsSidebarOpen(!isSidebarOpen);
-    };
-
     return (
-        <div className={`App ${isSidebarOpen ? 'sidebar-open' : ''}`}>
+        <div>
             <Main />
         </div>
     );
